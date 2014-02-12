68cde77b37bba47e80708552853ed3d8d473cbc1
arnrus09 <arnrus09@evergreen.edu>
2014-02-11 17:04:05 -0800

    more fragments

--- helloTest.java/helloTest/java/Searcher.java
+++ helloTest.java/helloTest/java/Searcher.java
@@ -8,8 +8,8 @@ import java.io.*;
 //this method thanks to Pavel Repin
 public class Searcher {
 	
-	static String convertStreamToString(OutputStream is) {
-	    Scanner s = new Scanner((Readable) is).useDelimiter("\\A");
+	static String convertStreamToString(InputStream is) {
+	    Scanner s = new Scanner(is).useDelimiter("\\A");
 	    return s.hasNext() ? s.next() : "";
 	}
 	
@@ -18,8 +18,8 @@ public static String[] tag(String sentence) throws IOException{
 	File home = new File("/Users/russellarnold");
 	List<String> theArgs = new ArrayList<String>();
 	theArgs.add("python");
-	theArgs.add("getTagsTagger.py");
-	//theArgs.add("testingpy.py");
+	//theArgs.add("getTagsTagger.py");
+	theArgs.add("testingpy.py");
 	theArgs.add("still");
 	theArgs.add("working");
 	//for( String r: sentence.split(" "))
@@ -31,7 +31,7 @@ public static String[] tag(String sentence) throws IOException{
 	
 	pb.directory(home);
 	 Process p = pb.start();
-	String temp = convertStreamToString(p.getOutputStream());
+	String temp = convertStreamToString(p.getInputStream());
 	System.out.println(temp);
 	String[] tagged = temp.split(",");
 	System.out.println(tagged.toString());
